0a45e7e3129ee15f5a2c2a37a547598469a41fc1
Karol <karolgar2307@gmail.com>
2026-05-23 09:38:49 -0500

    pokemo3

--- src/db.js
+++ src/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise');
 const useSsl = process.env.DB_SSL === 'true';
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT || 3306),
-  database: process.env.DB_NAME || 'pokemon_db',
-  user: process.env.DB_USER || 'pokemon_user',
-  password: process.env.DB_PASSWORD || 'pokemon_pass',
+  host: process.env.DB_HOST || 'kodama.proxy.rlwy.net',
+  port: Number(process.env.DB_PORT || 32314),
+  database: process.env.DB_NAME || 'railway',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'pQAAGmYplrOrbbsfbieiicIdMSeiyrHV',
   ssl: useSsl ? { rejectUnauthorized: false } : undefined,
   waitForConnections: true,
   connectionLimit: 5,
